56945e809e2792b4d63862f5fa04d0e88b904883
lecaros <jose.lecaros@gmail.com>
2021-03-10 16:30:36 -0500

    add publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
     kotlin("jvm") version "1.4.31"
+
+    id("maven-publish")
+    id("java-library")
 }
 
 group = "com.merkenlabs.googleapiwrapper.docs"
 version = "0.1.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
     mavenCentral()
@@ -11,4 +15,30 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
+
+    implementation("com.google.apis:google-api-services-docs:v1-rev20210302-1.31.0")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
+}
+
+tasks.getByName<Jar>("jar"){
+    enabled = true
+}
+
+java {
+    withSourcesJar()
+}
+
+publishing{
+    publications{
+        create<MavenPublication>("google-docs-api-wrapper"){
+            from(components["java"])
+        }
+    }
 }
