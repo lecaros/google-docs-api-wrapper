ebe914e2608b5a1f49a6fc084a7d659797c02384
lecaros <jose.lecaros@gmail.com>
2021-05-26 16:31:17 -0500

    add insertImage

--- src/main/kotlin/com/merkenlabs/googleapiwrapper/docs/IDocsServiceWrapper.kt
+++ src/main/kotlin/com/merkenlabs/googleapiwrapper/docs/IDocsServiceWrapper.kt
@@ -2,6 +2,8 @@ package com.merkenlabs.googleapiwrapper.docs
 
 import com.google.api.services.docs.v1.Docs
 import com.google.api.services.docs.v1.model.Document
+import com.google.api.services.docs.v1.model.Location
+import com.google.api.services.docs.v1.model.Size
 
 interface IDocsServiceWrapper {
     fun replaceTextInFile(fileId: String, oldText: String, newText: String)
@@ -9,4 +11,5 @@ interface IDocsServiceWrapper {
     fun replaceImage(fileId: String, imageObjectId: String, newImageUri: String)
     fun getDocumentById(fileId: String): Document
     fun getDocsService(): Docs
+    fun insertImage(fileId: String, newImageUri: String, location: Location, size: Size?)
 }
